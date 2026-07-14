f3c83ce855e26e737a352a21858378448a3f7f40
JhoelRm <jhoelhramirez923@gmail.com>
2026-07-14 03:58:38 -0500

    local host removed

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,10 +1,10 @@
-// export const environment = {
-//   production: true,
-//   apiUrl: 'https://timorabackend.onrender.com/api',
-//   authUrl: 'https://timorabackend.onrender.com/auth'
-// };
 export const environment = {
   production: true,
-  apiUrl: 'http://localhost:8080/api',
-  authUrl: 'http://localhost:8080/auth'
+  apiUrl: 'https://timorabackend.onrender.com/api',
+  authUrl: 'https://timorabackend.onrender.com/auth'
 };
+// export const environment = {
+//   production: true,
+//   apiUrl: 'http://localhost:8080/api',
+//   authUrl: 'http://localhost:8080/auth'
+// };
